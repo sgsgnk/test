636809d2307ac0eafc0c48ea32d78a779854f750
ninotani_katou <user@user-no-MacBook-Pro-28.local>
2015-04-16 13:38:15 +0900

    150416 お昼までにできたソースコードです。

--- c005.php
+++ c005.php
@@ -1,16 +1,15 @@
-/**********************************/
-動作確認では答えは判定できているのですが、
-for文が間違っていて２重表示されてしまいます。
-デバック中です。
-/**********************************/
 <?php
+//動作確認では答えは判定できているのですが、
+//for文が間違っていて２重表示されてしまいます。
+//デバック中です。
+
 //IPアドレスの数
 $num = fgets(STDIN);
 for($i = 0; $i < $num; $i++){
     $ipadd = fgets(STDIN);
     $ipans = filter_var($ipadd, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
-    //var_dump($value);
-    if($value = "false"){
+    var_dump($ipans);
+    if($ipans == false){
         echo "False\n";
         
         }
