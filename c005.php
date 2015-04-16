70c44f67fe9024d4075a0a07dbf08c026d3d4a6c
ninotani_katou <user@user-no-MacBook-Pro-28.local>
2015-04-16 13:01:51 +0900

    動作確認の結果答え自体はあっているのですがforの条件が悪く答えが２重表示されてしまいます。

--- c005.php
+++ c005.php
@@ -1,23 +1,14 @@
 <?php
-//数習得
+//IPアドレスの数
 $num = fgets(STDIN);
-//配列にIPアドレスを入れる
-for($i =0; $i < $num; $i++){
-$ipadd[] = fgets(STDIN);
-//配列の[i]番目を抜き取り分割する
-$ipcheck = explode('.' , $ipadd[$i]);
-//var_dump($ipcheck);
-//配列4つ以上はFalse
- $count = count($ipcheck);
-//print $count;
-//配列の数が4個以上だったらFalse
-if($count > 4){
-$ipans = "False";
- }else if($ipcheck[$i] >256 && empty($ipcheck[$i])){		//255以上だったらFalse
- $ipans = "False";
- }else{
- $ipans = "True";
- }
-echo $ipans."¥n";
+for($i = 0; $i < $num; $i++){
+    $ipadd = fgets(STDIN);
+    $ipans = filter_var($ipadd, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+    //var_dump($value);
+    if($value = "false"){
+        echo "False\n";
+        
+        }
+        echo "True\n";
 }
 ?>
